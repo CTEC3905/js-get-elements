e3ae01c19d28a1414901fa433edd492c17d64bf8
Fania Ra <fania@web.de>
2017-10-19 19:12:46 +0100

    add explanations

--- js/scripts.js
+++ js/scripts.js
@@ -2,39 +2,43 @@
 
 let spID = document.getElementById("special");
 let h2ALL = document.querySelectorAll("h2");
+let pspanALL = document.querySelectorAll("p span");
 let pALL = document.getElementsByTagName("p");
 let classALL = document.getElementsByClassName("foo");
-let options = document.getElementById("options-form");
 let pBTN = document.getElementById("p-button");
 let h2BTN = document.getElementById("h2-button");
 let formBTN = document.getElementById("form-button");
 let classBTN = document.getElementById("class-button");
 let idBTN = document.getElementById("id-button");
+let pspBTN = document.getElementById("psp-button");
 
-h2BTN.addEventListener("mouseover", function(e){
-  for(i=0;i<h2ALL.length;i++){
-    h2ALL[i].classList.add("highlight");
-  } e.preventDefault();}, false);
-h2BTN.addEventListener("mouseout", function(e){
-  for(i=0;i<h2ALL.length;i++){
-    h2ALL[i].classList.remove("highlight");
-  } e.preventDefault();}, false);
+let toggle = false; // unclicked
 
-pBTN.addEventListener("mouseover", function(e){
-  for(i=0;i<pALL.length;i++){
-    pALL[i].classList.add("highlight");
-  } e.preventDefault();}, false);
-pBTN.addEventListener("mouseout", function(e){
-  for(i=0;i<pALL.length;i++){
-    pALL[i].classList.remove("highlight");
-  } e.preventDefault();}, false);
+function makeChange(src, tar){
+  if (toggle) {
+    for(i=0;i<tar.length;i++){
+      tar[i].classList.remove("highlight");
+    }
+    toggle = false; // unclicked
+    src.classList.remove("toggle");
+  } 
+  else {
+    for(i=0;i<tar.length;i++){
+      tar[i].classList.add("highlight");
+    }
+    toggle = true; // clicked
+    src.classList.add("toggle");
+  }
+};
 
-formBTN.addEventListener("mouseover", function(e){
-  options.classList.add("highlight");
-  e.preventDefault();}, false);
-formBTN.addEventListener("mouseout", function(e){
-  options.classList.remove("highlight");
-  e.preventDefault();}, false);
+h2BTN.addEventListener("click", function(){
+  makeChange(h2BTN, h2ALL);}, false);
+
+pBTN.addEventListener("click", function(){
+  makeChange(pBTN, pALL);}, false);
+
+pspBTN.addEventListener("click", function(){
+  makeChange(pspBTN, pspanALL);}, false);
 
 classBTN.addEventListener("mouseover", function(e){
   for(i=0;i<classALL.length;i++){
